dbb776fb9d1d1dd277b0a2630388ab0e259268ee
takobrown <94686168+takobrown@users.noreply.github.com>
2023-05-27 16:33:53 +0000

    create user

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ import Dashboard from './components/Dashboard';
 import About from './components/About';
 import CatsDetail from './components/CatsDetail';
 import NewCats from './components/NewCats';
-import Login from './components/Login';
+import NewUsers from './components/NewUsers';
+
 
 
 const { Header, Content, Footer } = Layout;
@@ -24,7 +25,7 @@ export default function App() {
             <Link to="/dashboard">Dashboard</Link>
             <Link to="/about">About</Link>
             <Link to="/newcats">New Cats</Link>
-            <Link to="/login">Login</Link>
+            <Link to="/newusers">Register</Link>
           </Space>
         </nav>
       </Header>
@@ -35,7 +36,7 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/a/:aid" element={<CatsDetail />} />
           <Route path="/newcats" element={<NewCats />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/newUsers" element={<NewUsers />} />
         </Routes>
       </Content>
       <Footer>
---
+++ src/components/NewUsers.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Form, Input, Button, message } from 'antd';
+import axios from "axios";
+import { api } from './common/http-common';
+
+const NewUsers = () => {
+  const handleFormSubmit = async (values: any) => {
+    const { username, password, email, signupcode } = values;
+
+    if (!username || !password || !email || !signupcode) {
+      message.error('Please fill in all the required fields');
+      return;
+    }
+
+    if (signupcode !== 'pet') {
+      message.error('You are not allowed to register an account. Try Again or Leave it!');
+      return;
+    }
+
+    try {
+      await axios.post(`${api.url}/users`, {
+        ...values
+      });
+      message.success('Successful! Go to "Login" page to login.');
+    } catch (error) {
+      console.error('Error creating user:', error);
+    }
+  };
+
+  return (
+    <Form name="newUser" onFinish={handleFormSubmit}>
+      <Form.Item name="firstname" label="First Name">
+        <Input />
+      </Form.Item>
+      <Form.Item name="lastname" label="Last Name">
+        <Input />
+      </Form.Item>
+      <Form.Item name="username" label="Username" rules={[{ required: true, message: 'Username cannot be empty!' }]}>
+        <Input />
+      </Form.Item>
+      <Form.Item name="password" label="Password" rules={[{ required: true, message: 'Password cannot be empty!' }]}>
+        <Input.Password />
+      </Form.Item>
+      <Form.Item name="email" label="Email" rules={[{ required: true, message: 'Email cannot be empty' }]}>
+        <Input type="email" />
+      </Form.Item>
+      <Form.Item name="avatarurl" label="Avatar URL">
+        <Input />
+      </Form.Item>
+      <Form.Item name="signupcode" label="Sign-up Code" rules={[{ required: true, message: 'Sign-up Code can be got from your supervisor.' }]}>
+        <Input />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">Submit</Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default NewUsers;
+
