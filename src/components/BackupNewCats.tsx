f1f7b9ea40ab03129339def192ec577c6aed6599
takobrown <94686168+takobrown@users.noreply.github.com>
2023-05-28 16:06:32 +0000

    minor update

---
+++ src/components/BackupNewCats.tsx
@@ -0,0 +1,134 @@
+/*import React from "react";
+import { Form, Input, Button } from 'antd';
+import { Buffer } from 'buffer';
+import axios from "axios";
+import { api } from './common/http-common';
+
+const { TextArea } = Input
+
+const NewCats = () => {
+  const username = "alice";
+  const password = "alice1234";
+  // Create token by username:password
+  const access_token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64');
+  localStorage.setItem('atoken', access_token);
+  
+  const handleFormSubmit = (values: any) => {
+    const t = values.title;
+    const c = values.context;
+    console.log(values, t, c);
+    const postCats = {
+      title: t,
+      allText: c,
+      authorID: 1
+    }
+    
+    // Post request
+    axios.post(`${api.url}/cats`, postCats, {
+      headers: {
+        'Authorization': `Basic ${localStorage.getItem('atoken')}`
+      }
+    }).then((res)=> {
+      console.log(res.data);
+    });
+    
+  }
+
+  const contentRules = [
+    {required: true, message: 'Please input somethings'}    
+  ]
+  
+  return (
+    <Form name="article" onFinish={(values)=>handleFormSubmit(values)}>
+      <Form.Item name="title" label="Title" rules={contentRules}>
+        <Input />
+      </Form.Item>
+      <Form.Item name="context" label="Context" rules={contentRules}>
+        <TextArea rows={4} />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">Submit</Button>
+      </Form.Item>
+    
+    </Form>
+  )
+}
+
+export default NewCats;*/
+import React, { useState } from "react";
+import { Form, Input, Button } from 'antd';
+import { Buffer } from 'buffer';
+import axios from "axios";
+import { api } from './common/http-common';
+
+const { TextArea } = Input;
+
+const NewCats = () => {
+  const [image, setImage] = useState<File | null>(null); // State to store the uploaded image
+
+  //const username = "alice";
+  //const password = "alice1234";
+
+
+  /* Create token by username:password
+  const access_token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64');
+  localStorage.setItem('atoken', access_token);*/
+              
+  const handleFormSubmit = async (values: any) => {
+    
+    const t = values.title;
+    const c = values.context;
+
+    console.log(values, t, c);
+
+
+    const postCats = {
+      title: t,
+      allText: c,
+      postid: 1
+    };
+
+    // Post request
+  /*  try {
+      const response = await axios.post(`${api.url}/cats`, postCats, {
+        headers: {
+          'Authorization': `Basic ${localStorage.getItem('atoken')}`
+        }
+      });
+    }
+      const catId = response.data.id;
+  }*/
+      try {
+     await  axios.post(`${api.url}/cats`, postCats, {
+        headers: {
+          'Authorization': `Basic ${localStorage.getItem('atoken')}`
+        }
+      });
+    } catch (error) {
+      console.error('Error creating user:', error);
+    }
+  };
+    
+  
+  const contentRules = [
+    { required: true, message: 'Please input something' }
+  ];
+
+ 
+  return (
+    <Form name="article" onFinish={(values) => handleFormSubmit(values)}>
+      <Form.Item name="title" label="Title" rules={contentRules}>
+        <Input />
+      </Form.Item>
+      <Form.Item name="context" label="Context" rules={contentRules}>
+        <TextArea rows={4} />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">Submit</Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default NewCats;
+
--- src/components/NewCats.tsx
+++ src/components/NewCats.tsx
@@ -1,127 +1,66 @@
-/*import React from "react";
+import React, { useState, ChangeEvent } from "react";
 import { Form, Input, Button } from 'antd';
-import { Buffer } from 'buffer';
-import axios from "axios";
-import { api } from './common/http-common';
-
-const { TextArea } = Input
-
-const NewCats = () => {
-  const username = "alice";
-  const password = "alice1234";
-  // Create token by username:password
-  const access_token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64');
-  localStorage.setItem('atoken', access_token);
-  
-  const handleFormSubmit = (values: any) => {
-    const t = values.title;
-    const c = values.context;
-    console.log(values, t, c);
-    const postCats = {
-      title: t,
-      allText: c,
-      authorID: 1
-    }
-    
-    // Post request
-    axios.post(`${api.url}/cats`, postCats, {
-      headers: {
-        'Authorization': `Basic ${localStorage.getItem('atoken')}`
-      }
-    }).then((res)=> {
-      console.log(res.data);
-    });
-    
-  }
-
-  const contentRules = [
-    {required: true, message: 'Please input somethings'}    
-  ]
-  
-  return (
-    <Form name="article" onFinish={(values)=>handleFormSubmit(values)}>
-      <Form.Item name="title" label="Title" rules={contentRules}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="context" label="Context" rules={contentRules}>
-        <TextArea rows={4} />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">Submit</Button>
-      </Form.Item>
-    
-    </Form>
-  )
-}
-
-export default NewCats;*/
-import React, { useState } from "react";
-import { Form, Input, Button } from 'antd';
-import { Buffer } from 'buffer';
 import axios from "axios";
 import { api } from './common/http-common';
 
 const { TextArea } = Input;
 
-const NewCats = () => {
-  const [image, setImage] = useState<File | null>(null); // State to store the uploaded image
+interface NewCatsProps {
+  basicAuth: string;
+}
 
-  //const username = "alice";
-  //const password = "alice1234";
+const NewCats: React.FC<NewCatsProps> = ({ basicAuth }) => {
+  const [title, setTitle] = useState<string>("");
+  const [context, setContext] = useState<string>("");
+  const [imageBase64, setImageBase64] = useState<string | null>(null);
 
-
-  /* Create token by username:password
-  const access_token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64');
-  localStorage.setItem('atoken', access_token);*/
-              
-  const handleFormSubmit = async (values: any) => {
-    
-    const t = values.title;
-    const c = values.context;
-
-    console.log(values, t, c);
-
-
+  const handleFormSubmit = async () => {
     const postCats = {
-      title: t,
-      allText: c,
+      title,
+      allText: context,
+      imageBase64,
       postid: 1
     };
 
-    // Post request
-  /*  try {
-      const response = await axios.post(`${api.url}/cats`, postCats, {
+    try {
+      await axios.post(`${api.url}/cats`, postCats, {
         headers: {
           'Authorization': `Basic ${localStorage.getItem('atoken')}`
         }
       });
-    }
-      const catId = response.data.id;
-  }*/
-      try {
-     await  axios.post(`${api.url}/cats`, postCats, {
-        headers: {
-          'Authorization': `Basic ${localStorage.getItem('atoken')}`
-        }
-      });
+
+      console.log("Cat posted successfully!");
     } catch (error) {
-      console.error('Error creating user:', error);
+      console.error('Error creating cat:', error);
     }
   };
-    
-  
-  const contentRules = [
-    { required: true, message: 'Please input something' }
-  ];
 
- 
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files && e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const base64String = reader.result as string;
+        setImageBase64(base64String);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setImageBase64(null);
+    }
+  };
+
+  const contentRules = [{ required: true, message: 'Please input something' }];
+
   return (
-    <Form name="article" onFinish={(values) => handleFormSubmit(values)}>
+    <Form name="article" onFinish={handleFormSubmit}>
       <Form.Item name="title" label="Title" rules={contentRules}>
-        <Input />
+        <Input value={title} onChange={(e) => setTitle(e.target.value)} />
       </Form.Item>
       <Form.Item name="context" label="Context" rules={contentRules}>
-        <TextArea rows={4} />
+        <TextArea rows={4} value={context} onChange={(e) => setContext(e.target.value)} />
+      </Form.Item>
+      <Form.Item name="image" label="Image">
+        <Input type="file" onChange={handleImageChange} />
       </Form.Item>
       <Form.Item>
         <Button type="primary" htmlType="submit">Submit</Button>
@@ -131,4 +70,3 @@ const NewCats = () => {
 };
 
 export default NewCats;
-
